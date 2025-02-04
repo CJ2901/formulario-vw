34b35b3f4f328ec9215711194aa9c5ae6c538504
CJ2901 <christian.magallanes@alum.udep.edu.pe>
2025-02-04 14:53:48 -0500

    Upload 4F version 1.0.1

---
+++ src/data/fakePlacasdata.tsx
@@ -0,0 +1,110 @@
+const fakePlacasData = [ 
+    {
+      placa: 'ABC123',
+      bastidor: '3VVHJ65N9PM070726',
+      modeloVersion: 'T-CROSS ALLSPACE LIFE 250TSI 1.4L 2WD DSG'
+    },
+    {
+      placa: 'CJV685',
+      bastidor: '3VVHJ65N9PM070726',
+      modeloVersion: 'TIGUAN ALLSPACE LIFE 250TSI 1.4L 2WD DSG'
+    },
+    {
+      placa: 'BZS744',
+      bastidor: '8AWDW22H8RA018678',
+      modeloVersion: 'AMAROK COMFORTLINE 3.0 V6 TDI 4x4 - AT, 258 CV'
+    },
+    {
+      placa: 'CMI475',
+      bastidor: '3VVKP6B20RM084161',
+      modeloVersion: 'TAOS TRENDLINE 250 TSI TIP'
+    },
+    {
+      placa: 'CMI285',
+      bastidor: '3VV9P6B23RM082452',
+      modeloVersion: 'TAOS HIGHLINE 250 TSI TIP'
+    },
+    {
+      placa: 'CMW434',
+      bastidor: '9BWCH6CH0RP069033',
+      modeloVersion: 'NIVUS COMFORTLINE 200 TSI TIP'
+    },
+    {
+      placa: 'CNE452',
+      bastidor: '3VV9P6B22RM083804',
+      modeloVersion: 'TAOS HIGHLINE 250 TSI TIP'
+    },
+    {
+      placa: 'CNE534',
+      bastidor: '3VVRH65N5RM118354',
+      modeloVersion: 'TIGUAN ALLSPACE R-LINE 240TSI 2.0L 4MOTION TIP'
+    },
+    {
+      placa: 'CNE378',
+      bastidor: '9BWCH6CH0RP068772',
+      modeloVersion: 'NIVUS COMFORTLINE 200 TSI TIP'
+    },
+    {
+      placa: 'CNK030',
+      bastidor: 'WVGZZZCA1RC599567',
+      modeloVersion: 'TERAMONT PREMIUM 2.0 TSI - AT'
+    },
+    {
+      placa: 'CPN351',
+      bastidor: '3VV9P6B21RM096835',
+      modeloVersion: 'TAOS HIGHLINE 250 TSI TIP'
+    },
+    {
+      placa: 'CNO024',
+      bastidor: '9BWBH6BF6S4005419',
+      modeloVersion: 'T-CROSS COMFORTLINE 200 TSI TIP'
+    },
+    {
+      placa: 'CAW757',
+      bastidor: '8AWDW22H9RA020021',
+      modeloVersion: 'AMAROK HIGHLINE 3.0 V6 TDI 4x4 - AT, 258 CV'
+    },
+    {
+      placa: 'BZV790',
+      bastidor: '8AWDW22H9RA018561',
+      modeloVersion: 'AMAROK HIGHLINE 3.0 V6 TDI 4x4 - AT'
+    },
+    {
+      placa: 'BYX574',
+      bastidor: '3VVRH65N0NM044352',
+      modeloVersion: 'TIGUAN ALLSPACE R-LINE 240TSI 2.0L 4MOTION TIP'
+    },
+    {
+      placa: 'CPE654',
+      bastidor: '9BWCH6CH6RP037817',
+      modeloVersion: 'NIVUS HIGHLINE 200 TSI TIP'
+    },
+    {
+      placa: 'Z7Q146',
+      bastidor: '9BWDL5BZ2NP038822',
+      modeloVersion: 'VIRTUS COMFORTLINE 1.6 MEC'
+    },
+    {
+      placa: 'Z7Q145',
+      bastidor: '9BWDL5BZ5NP047823',
+      modeloVersion: 'VIRTUS COMFORTLINE 1.6 MEC'
+    },
+    {
+      placa: 'TCN875',
+      bastidor: 'WV1ZZZ2HZPA000426',
+      modeloVersion: 'AMAROK HIGHLINE 3.0 V6 TDI 4x4 - AT, 258 CV'
+    },
+    {
+      placa: 'T6X161',
+      bastidor: '9BWCH6CH5PP023128',
+      modeloVersion: 'NIVUS HIGHLINE 200 TSI TIP'
+    },
+    {
+      placa: 'TEL848',
+      bastidor: '8AWDW22H4RA019116',
+      modeloVersion: 'AMAROK COMFORTLINE 3.0 V6 TDI 4x4 - AT, 258 CV'
+    }
+];
+
+
+export default fakePlacasData;
